8cfd7e2b5eee69a12f241ab5a1be496989273c62
hammondjn <jessica@mobiusconsortium.org>
2012-02-29 15:52:15 -0600

    More work on the qr code script for Columbia College

--- qr.js
+++ qr.js
@@ -1,14 +1,27 @@
+var qrlink
+var linkText
+var qrCode
+
 function linkQR(){
   
-$(".bibLinks a").each(function() {
-    var qrlink = $(this).attr("href");
-    if( qrlink.indexOf("ccis") != -1 ) {
-      qrCode = '<img src="http://chart.apis.google.com/chart?chs=125x125&cht=qr&chl=' + qrlink + '">';
-      $("#qr").html(qrCode);
-    }else{
-    }
+function create_qr_code(index) {
+    qrlink = $(".bibLinks a:eq(" + index + ")").attr("href");
+}
+  
+$(".bibLinks a").each(function(index) {
+    create_qr_code(index);
+    linkText = $(this).text()
+    $("<input type='radio' name='item' />" + linkText + "<br />").appendTo("#qrChoice")
+}); 
+
+$("#qrChoice input").change(function () {
+    index = $("#qrChoice input").index(this);
+    create_qr_code(index);
+    var qrCode = '<img src="http://chart.apis.google.com/chart?chs=125x125&cht=qr&chl=' + qrlink + '">';
+    $("#qr").html(qrCode);
 });
 
-
+var qrCode = '<img src="http://chart.apis.google.com/chart?chs=125x125&cht=qr&chl=' + qrlink + '">';
+$("#qr").html(qrCode);
       
 }
